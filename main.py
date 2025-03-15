f319b549a3d820bf999b1898ff3b7bd207616778
Vorobaa <vorobaaa72@gmail.com>
2025-03-15 11:11:55 +0200

    Crash message

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from email.policy import default
+
 import pygame
 import pygame as pg
 import random
@@ -24,7 +27,8 @@ images_dict = {
     'hotel':pg.transform.scale(pg.image.load('img/hotel.png'),(80, 80)),
     'pas': pg.image.load('img/passenger.png'),
     'screen': pg.image.load('img/screenshot.jpg'),
-    't_bg': pg.transform.scale(pg.image.load('img/taxi_background.png'), (80, 45))
+    't_bg': pg.transform.scale(pg.image.load('img/taxi_background.png'), (80, 45)),
+    'parking': pg.transform.scale(pg.image.load('img/Image20250301115336.png'), (80, 45))
 
 }
 
@@ -41,10 +45,52 @@ hotel_positions = [
     (60, 250),
     (555, 250)
 ]
+pas_img = images_dict['pas']
+pas_rect = pas_img.get_rect()
+# pas_positions = [
+#     (60, 30),
+#     (555, 30),
+#     (60, 365),
+#     (555, 250)
+# ]
+
+
 hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
 
+pas_rect.x, pas_rect.y = random.choice(hotel_positions)
+pas_rect.y += hotel_rect.height
+# while (pas_rect.x, pas_rect.y) == (hotel_rect.x, hotel_rect.y):
+#     pass
+
+actions = [0, 1, 2, 3] # 0 - right, 1 - left, 2 - up, 3 - bottom
+Q_table = defaultdict(lambda: [0, 0, 0, 0]) # (300, 300) : [-2, -3, 5, 3]
+
+learning_rate = 0.9
+discount_factor = 0.9
+epsilon = 0.1
+
+def draw_message(text, color):
+    font = pg.font.SysFont(None, 36)
+    message = font.render(text, True, color)
+    screen.blit(message, (300, 150))
+    pg.display.flip()
+    pg.time.delay(1500)
+
+
+
+def is_crash():
+    for x in range(player_rect.x, player_rect.topright[0], 1):
+        for y in range(player_rect.y, player_rect.bottomleft[1], 1):
+            try:
+                if screen.get_at((x,y)) == (220, 215, 177):
+                    return True
+            except IndexError:
+                print("pixel index out of range")
+    return False
+
+
 #parking
-parking_img = images_dict['t_bg']
+parking_img = images_dict['parking']
 parking_rect = parking_img.get_rect()
 parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
 
@@ -101,6 +147,35 @@ while run:
     x_direction = 0
     y_direction = 0
 
+    if is_crash():
+        print("IS CRASH")
+        draw_message("You crash!!", pg.Color('red'))
+        player_view = 'rear'
+        player_rect.x = 300
+        player_rect.y = 300
+
+        hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
+        parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
+        pas_rect.x, pas_rect.y = random.choice(hotel_positions)
+        pas_rect.y += hotel_rect.height
+        continue
+
+
+    if player_rect.colliderect(pas_rect):
+        pas_rect.x, pas_rect.y = player_rect.x, player_rect.y
+
+    if parking_rect.contains(player_rect):
+        draw_message("You win!!", pg.Color('green'))
+        player_view = 'rear'
+        player_rect.x = 300
+        player_rect.y = 300
+
+        hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
+        parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
+        pas_rect.x, pas_rect.y = random.choice(hotel_positions)
+        pas_rect.y += hotel_rect.height
+        continue
+
 
     # Відображення
     screen.fill(background_color)
@@ -110,7 +185,7 @@ while run:
     screen.blit(images_dict['player'][player_view], player_rect)
     screen.blit(images_dict['hotel'], hotel_rect)
     screen.blit(parking_img, parking_rect)
-
+    screen.blit(images_dict['pas'], pas_rect)
 
     pygame.display.flip()
 
